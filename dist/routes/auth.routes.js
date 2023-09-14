a28a157cedead0d754a639c56d3a4c88b26ce2df
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2023-09-14 05:34:00 -0300

    fix: delete dist

--- dist/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authentication_controller_1 = require("../controllers/authentication.controller");
-const authRoute = (0, express_1.Router)();
-authRoute.post("/login", authentication_controller_1.loginController);
-authRoute.post("/signup", authentication_controller_1.singnupController);
-exports.default = authRoute;
